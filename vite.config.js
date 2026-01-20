fce5a4846680dc82e91e9da5f2d543f8722532e0
Kevin.M <kema2501@student.miun.se>
2026-01-20 15:19:43 +0100

    Hantering felmeddelande vite.congig samt uppdatering av index.html

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
-import {resolve } from "path";
+import { resolve } from "path";
 
 export default defineConfig({
-    build: {rollupOptions: {
-        input: {
-            main: resolve(__dirname, "index.html"),
-            bilder: resolve(dirname, "bilder.html"),
-            arbetsprocess: resolve(dirname, "arbetsprocess.html")
-        }
-    }}
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+        bilder: resolve("bilder.html"),
+        arbetsprocess: resolve("arbetsprocess.html")
+      }
+    }
+  }
+});
