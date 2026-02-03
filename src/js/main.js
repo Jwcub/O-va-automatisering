7b6835af4dcf58c989bf333dcd4fdc892c5f0bf7
Kevin.M <kema2501@student.miun.se>
2026-02-03 14:32:06 +0100

    Inläsning av kurser samt sortering i bokstavsordning

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,63 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
+let allCourses = [];
 
+document.addEventListener("DOMContentLoaded", async () => {
+    getData();
+    pageTheme();
+
+    document.getElementById("course-code").addEventListener("click", () => {
+        const sortByCode = [...allCourses].sort((a, b) => a.code.localeCompare(b.code));
+        displayCourses(sortByCode);
+    });
+
+    document.getElementById("course-name").addEventListener("click", () => {
+        const sortByName = [...allCourses].sort((a, b) => a.coursename.localeCompare(b.coursename));
+        displayCourses(sortByName);
+    });
+
+    document.getElementById("course-progression").addEventListener("click", () => {
+        const sortByProgression = [...allCourses].sort((a, b) => a.progression.localeCompare(b.progression));
+        displayCourses(sortByProgression);
+    });
+});
+
+async function getData() {
+    const url = "https://webbutveckling.miun.se/files/ramschema.json";
+
+    // Anopa och läs ut kursdata
+    try {
+        const response = await fetch(url);
+        const courses = await response.json();
+
+        // Lagra alla kurser globalt
+        allCourses = courses;
+
+        displayCourses(courses);
+
+    } catch(error) {
+        console.error("Fel " + error)
+    }
+}
+
+function displayCourses(courses) {
+
+    const courseTableEl = document.getElementById("course-tbody");
+    courseTableEl.innerHTML = "";
+
+    // Loopa genom alla kurser och skriv ut
+    courses.forEach(course => {
+        courseTableEl.innerHTML += `
+            <tr>
+                <td>${course.code}</td>
+                <td>${course.coursename}</td>
+                <td>${course.progression}</td>
+            </tr>
+        `;
+    });
+}
+ 
+function pageTheme() {
     const selectedTheme = document.getElementById("theme-select");
     const root = document.documentElement;
 
@@ -24,5 +80,6 @@ document.addEventListener("DOMContentLoaded", () => {
         root.style.colorScheme = "light dark";
         selectedTheme.value = "light dark";
     }
+}
 
-});
+
